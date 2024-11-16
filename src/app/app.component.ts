5b99e37f8a04b369099a266c57af5f56b1cdb0ed
Marine Van Hees <vanheesmarine@gmail.com>
2024-11-16 23:55:02 +0100

    first card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { FeedbackComponent } from './feedback/feedback.component';
+import { DescriptionComponent } from './description/description.component';
+import { CardsComponent } from './cards/cards.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    FeedbackComponent],
+    FeedbackComponent,
+    DescriptionComponent,
+    CardsComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
