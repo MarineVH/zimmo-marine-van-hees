3e9fb87b62ce3228ac3fcbc9d327f7901db2c237
Marine Van Hees <vanheesmarine@gmail.com>
2024-11-17 01:21:51 +0100

    data for more cards

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,15 +1,40 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../card/card.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-cards',
   standalone: true,
   imports: [
+    NgFor,
     CardComponent
   ],
   templateUrl: './cards.component.html',
   styleUrl: './cards.component.css'
 })
 export class CardsComponent {
+
+  cards = [
+    {
+      title: '6 makkelijke manieren om voor een huis te sparen',
+      description: 'Een huis kopen een onbereikbare toekomstdroom? Dat hoeft helemaal niet!',
+      imageUrl: 'assets/articleImage/piggybank.png',
+    },
+    {
+      title: '6 makkelijke manieren om voor een huis te sparen',
+      description: 'Een huis kopen een onbereikbare toekomstdroom? Dat hoeft helemaal niet!',
+      imageUrl: 'assets/articleImage/piggybank.png',
+    },
+    {
+      title: '6 makkelijke manieren om voor een huis te sparen',
+      description: 'Een huis kopen een onbereikbare toekomstdroom? Dat hoeft helemaal niet!',
+      imageUrl: 'assets/articleImage/piggybank.png',
+    },
+    {
+      title: '6 makkelijke manieren om voor een huis te sparen',
+      description: 'Een huis kopen een onbereikbare toekomstdroom? Dat hoeft helemaal niet!',
+      imageUrl: 'assets/articleImage/piggybank.png',
+    },
+  ];
 
 }
