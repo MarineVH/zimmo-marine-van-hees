28c9cfc3ebc4b90a2918198105a683a42ccad81e
Marine Van Hees <vanheesmarine@gmail.com>
2024-11-17 18:59:38 +0100

    error message and succes message

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { SmileyButtonComponent } from "../smiley-button/smiley-button.component";
-import { NgIf } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -8,6 +8,7 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [
     NgIf,
+    NgClass,
     FormsModule,
     SmileyButtonComponent],
   templateUrl: './feedback.component.html',
@@ -17,17 +18,28 @@ export class FeedbackComponent {
   showFeedback = false;
   feedbackText = '';
   selectedFeeling = '';
+  showError = false;
+  successMessage = '';
 
   openFeedback(feeling: string) {
     this.selectedFeeling = feeling;
     this.showFeedback = true;
+    this.successMessage = '';
+    this.showError = false;
   }
 
   submitFeedback() {
-    console.log('Feedback:', this.feedbackText);
-    console.log('Feeling:', this.selectedFeeling);
-    this.feedbackText = '';
-    this.selectedFeeling = '';
-    this.showFeedback = false;
+    if (!this.feedbackText.trim()) {
+      this.showError = true;
+      this.successMessage = '';
+    } else {
+      console.log('Feedback:', this.feedbackText);
+      console.log('Feeling:', this.selectedFeeling);
+      this.feedbackText = '';
+      this.selectedFeeling = '';
+      this.showFeedback = false;
+      this.showError = false;
+      this.successMessage = 'Feedback met succes verzonden!';
+    }
   }
 }
